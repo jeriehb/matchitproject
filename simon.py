64bce9e7632598cb19b148bbff0ef237b6c9a69f
simon <jeriehb5@gmail.com>
2019-08-05 11:04:47 +0200

    added hello world

---
+++ simon.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  5 10:58:50 2019
+
+@author: simon
+"""
+
+from numpy import *
+from matplotlib.pyplot import *
+print ("hello world")
